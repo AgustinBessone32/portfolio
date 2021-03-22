972d699c480ec2ebc20a3d3b6f3264632169ef3c
Agustin Bessone <bessone98@gmail.com>
2021-03-21 23:28:08 -0300

    reparando errores

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -13,24 +13,24 @@ const Contact = ()=> {
                     <div className='content-contact'>
                             <FaGoogle className='icn-contact' />
                             <p className='txt-contact'>bessone98@gmail.com</p>
-                        </div>
-                        <div className='content-contact'>
+                    </div>
+                    <div className='content-contact'>
                             <FaLinkedin className='icn-contact' />
                             <a href='https://www.linkedin.com/in/agustinbessone/' 
                                 className='txt-contact'
                                 target='_blank'> -<FaRegSmileWink className='smile'/>
                                 Visita  mi  Linkedin 
                                 <FaRegSmileWink className='smile'/>-</a>
-                        </div>
-                        <div className='content-contact'>
+                    </div>
+                    <div className='content-contact'>
                             <BsPersonSquare className='icn-contact' /> 
                             <a href={CV}
                                 download='CV Agustin Bessone.pdf'
                                 className='txt-contact'
                                 target='_blank'>- <FaRegSmileWink className='smile'/>
                                 Descarga mi CV<FaRegSmileWink className='smile'/> -</a>
-                        </div>
                     </div>
+                </div>
 
         </div>
     )
