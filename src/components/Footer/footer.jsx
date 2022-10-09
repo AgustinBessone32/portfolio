56e3b070f9fac63b33a232e45fefbbf5708f020a
Agustin Bessone <bessone98@gmail.com>
2022-10-09 20:15:57 -0300

    footer dinamico

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import './footer.css'
 import { AiOutlineCopyrightCircle } from 'react-icons/ai'
+import './footer.css'
 
 const Footer = () => {
+    const year = new Date().getFullYear()
+
+
     return(
         <div className='content-all-footer'>
             <div className='content-copy'>
-                <p> <AiOutlineCopyrightCircle />  Agustin Bessone, 2021</p>
+                <p> <AiOutlineCopyrightCircle />  Agustin Bessone, {year}</p>
             </div>
 
         </div>
